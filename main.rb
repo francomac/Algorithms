8d9d43f7e3577c64f59a708a77f24efb9ed98c82
Franco <francomac2086@hotmail.com>
2022-02-10 03:30:53 +0000

    partially applying harmlessRansomNote algorithm

--- main.rb
+++ main.rb
@@ -1,4 +1,21 @@
 #
+# You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+# Example
+
+# The maximum height candles are  units high. There are  of them, so return .
+
+# Function Description
+
+# Complete the function birthdayCakeCandles in the editor below.
+
+# birthdayCakeCandles has the following parameter(s):
+
+# int candles[n]: the candle heights
+# Returns
+
+# int: the number of candles that are tallest
+
 # Complete the 'birthdayCakeCandles' function below.
 #
 # The function is expected to return an INTEGER.
@@ -6,15 +23,14 @@
 #
 
 def birthdayCakeCandles(candles)
-    # Write your code here
-    # [9, 4 ,3, 2, 4, 7, 1, 7, 4]
+    # [9, 4 ,3, 9, 4, 7, 1, 7, 4]
     # 1. find out how many TIMES a NUM is in the array 
-    # { 9: 1, 4: 3, 3: 1, 2: 1, 7: 2}
-    # 2. find out what is the GREATER NUM with >1 TIMES { 9: 1 }
-    # return 1
+    # { 9: 2, 4: 3, 3: 1, 2: 1, 7: 2}
+    # 2. find out what is the GREATER NUM with >1 TIMES { 9: 2 }
+    # return 2
+
     candles_control = Hash.new
     biggerOne = 0
-    puts candles.length
 
     for i in candles do
         unless candles_control.member?(i)
@@ -24,7 +40,8 @@ def birthdayCakeCandles(candles)
         end
         biggerOne = i if (i > biggerOne)
     end
+    puts candles_control[biggerOne]
     candles_control[biggerOne]
 end
 
-birthdayCakeCandles [9, 4 ,3, 2, 4, 7, 1, 7, 4]
+birthdayCakeCandles [9, 4 ,3, 9, 4, 7, 1, 7, 4]
